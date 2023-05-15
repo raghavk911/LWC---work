a05e009a96068e54836af1b76fdb4b2a737f9c73
Raghavendra Kadam <raghav.k911@gmail.com>
2023-05-15 13:24:26 +0530

    added html in component

--- force-app/main/default/lwc/autoCompleteCombobox/autoCompleteCombobox.js
+++ force-app/main/default/lwc/autoCompleteCombobox/autoCompleteCombobox.js
@@ -1,3 +1,26 @@
 import { LightningElement } from 'lwc';
 
-export default class AutoCompleteCombobox extends LightningElement {}
+export default class AutoCompleteCombobox extends LightningElement {
+
+    statusOptions = [
+      { value: 'new', label: 'New', description: 'A new item' },
+      {
+          value: 'in-progress',
+          label: 'In Progress',
+          description: 'Currently working on this item',
+      },
+      {
+          value: 'finished',
+          label: 'Finished',
+          description: 'Done working on this item',
+      },
+  ];
+
+  value = 'new';
+
+  handleChange(event) {
+      // Get the string of the "value" attribute on the selected option
+      this.value = event.detail.value;
+  }
+
+}
