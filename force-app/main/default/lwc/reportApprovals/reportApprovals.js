77685cfc553d457b04abb1bb2cfc1cc87063b0a2
raghav.k911 <raghav.k911@gmail.com>
2023-07-10 18:37:00 +0530

    rendered record view form

--- force-app/main/default/lwc/reportApprovals/reportApprovals.js
+++ force-app/main/default/lwc/reportApprovals/reportApprovals.js
@@ -1,121 +1,151 @@
 import { LightningElement, track, wire } from 'lwc';
-import getAccounts from '@salesforce/apex/AccountDataController.getAccounts';
+//Import apex method 
+import fetchContacts from '@salesforce/apex/ContactDataController.fetchContacts';
 import { NavigationMixin } from 'lightning/navigation';
 import { deleteRecord } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { refreshApex } from '@salesforce/apex';
 
-const columns = [
-    { label: 'Name', fieldName: 'Name', sortable: "true" },
-    { label: 'Phone', fieldName: 'Phone', sortable: "true" },
-    { label: 'Type', fieldName: 'Type', sortable: "true" },
-    { label: 'Rating', fieldName: 'Rating', sortable: "true" },
-    { label: 'Industry', fieldName: 'Industry', sortable: "true" },
-    {
-        type: "button", label: 'View', initialWidth: 100, typeAttributes: {
-            label: 'View',
-            name: 'View',
-            title: 'View',
-            disabled: false,
-            value: 'view',
-            iconPosition: 'left',
-            iconName:'utility:preview',
-            variant:'Brand'
-        }
-    },
-    {
-        type: "button", label: 'Edit', initialWidth: 100, typeAttributes: {
-            label: 'Edit',
-            name: 'Edit',
-            title: 'Edit',
-            disabled: false,
-            value: 'edit',
-            iconPosition: 'left',
-            iconName:'utility:edit',
-            variant:'Brand'
-        }
-    },
-    {
-        type: "button", label: 'Delete', initialWidth: 110, typeAttributes: {
-            label: 'Delete',
-            name: 'Delete',
-            title: 'Delete',
-            disabled: false,
-            value: 'delete',
-            iconPosition: 'left',
-            iconName:'utility:delete',
-            variant:'destructive'
-        }
-    }
-];
+export default class DatatableWithPagination extends LightningElement {
+    
+    // JS Properties 
+    pageSizeOptions = [5, 10, 25, 50, 75, 100]; //Page size options
+    records = []; //All records available in the data table
+    columns = []; //columns information available in the data table
+    totalRecords = 0; //Total no.of records
+    pageSize; //No.of records to be displayed per page
+    totalPages; //Total no.of pages
+    pageNumber = 1; //Page number    
+    recordsToDisplay = []; //Records to be displayed on the page
+    @track isModalOpen = false;
+    @track objectName = 'Contact';
+    
 
-export default class ButtonsInLWC extends NavigationMixin(LightningElement) {
-    @track data;
-    @track wireResult;
-    @track error;
-    columns = columns;
+    //---------Test Sarjerao-----//
 
-    @wire(getAccounts)
-    wiredAccounts(result) {
-        this.wireResult = result;
-        if (result.data) {
-            this.data = result.data;
-        } else if (result.error) {
-            this.error = result.error;
-        }
+    @track recordId;
+
+    get bDisableFirst() {
+        return this.pageNumber == 1;
     }
 
-    callRowAction(event) {
-        const recId = event.detail.row.Id;
-        const actionName = event.detail.action.name;
-        if (actionName === 'Edit') {
-            this.handleAction(recId, 'edit');
-        } else if (actionName === 'Delete') {
-            this.handleDeleteRow(recId);
-        } else if (actionName === 'View') {
-            this.handleAction(recId, 'view');
-        }
+    get bDisableLast() {
+        return this.pageNumber == this.totalPages;
     }
 
-    handleAction(recordId, mode) {
-        this[NavigationMixin.Navigate]({
-            type: 'standard__recordPage',
-            attributes: {
-                recordId: recordId,
-                objectApiName: 'Account',
-                actionName: mode
+    // connectedCallback method called when the element is inserted into a document
+    connectedCallback() {
+        // set datatable columns info
+        this.columns =[
+            { label: 'Name', fieldName: 'Name', sortable: "true" },
+            { label: 'Phone', fieldName: 'Phone', sortable: "true" },
+            { label: 'Title', fieldName: 'Title', sortable: "true" },
+            { label: 'Email', fieldName: 'Email', sortable: "true" },
+
+            {
+                type: "button", label: 'View', initialWidth: 100, typeAttributes: {
+                    label: 'View',
+                    name: 'View',
+                    title: 'View',
+                    disabled: false,
+                    value: 'view',
+                    iconPosition: 'left',
+                    iconName:'utility:preview',
+                    variant:'Brand'
+                }
             }
-        })
-    }
+        ];
 
-    handleDeleteRow(recordIdToDelete) {
-        deleteRecord(recordIdToDelete)
-            .then(result => {
-                this.showToast('Success!!', 'Record deleted successfully!!', 'success', 'dismissable');
-                return refreshApex(this.wireResult);
-            }).catch(error => {
-                this.error = error;
+
+        // fetch contact records from apex method 
+        fetchContacts()
+            .then((result) => {
+                if (result != null) {
+                    console.log('RESULT--> ' + JSON.stringify(result));
+                    this.records = result;
+                    this.totalRecords = result.length; // update total records count                 
+                    this.pageSize = this.pageSizeOptions[0]; //set pageSize with default value as first option
+                    this.paginationHelper(); // call helper menthod to update pagination logic 
+                }
+            })
+            .catch((error) => {
+                console.log('error while fetch contacts--> ' + JSON.stringify(error));
             });
     }
 
-    showToast(title, message, variant, mode) {
-        const evt = new ShowToastEvent({
-            title: title,
-            message: message,
-            variant: variant,
-            mode: mode
-        });
-        this.dispatchEvent(evt);
+     // ON ROW ACTION BUTTON --- VIEW
+     openModal(event) {
+        // to open modal set isModalOpen tarck value as true
+        this.isModalOpen = true;
+        this.recordId = event.detail.row.Id;
+        console.log('test id---'+JSON.stringify(this.recordId));     
+    }
+    closeModal() {
+        // to close modal set isModalOpen tarck value as false
+        this.isModalOpen = false;
+    }
+    submitDetails() {
+        // to close modal set isModalOpen tarck value as false
+        //Add your code to call apex method or do some processing
+        this.isModalOpen = false;
     }
 
+    // PAGINATION
+    handleRecordsPerPage(event) {
+        this.pageSize = event.target.value;
+        this.paginationHelper();
+    }
+
+    previousPage() {
+        this.pageNumber = this.pageNumber - 1;
+        this.paginationHelper();
+    }
+
+    nextPage() {
+        this.pageNumber = this.pageNumber + 1;
+        this.paginationHelper();
+    }
+
+    firstPage() {
+        this.pageNumber = 1;
+        this.paginationHelper();
+    }
+
+    lastPage() {
+        this.pageNumber = this.totalPages;
+        this.paginationHelper();
+    }
+
+    // JS function to handel pagination logic 
+    paginationHelper() {
+        this.recordsToDisplay = [];
+        // calculate total pages
+        this.totalPages = Math.ceil(this.totalRecords / this.pageSize);
+        // set page number 
+        if (this.pageNumber <= 1) {
+            this.pageNumber = 1;
+        } else if (this.pageNumber >= this.totalPages) {
+            this.pageNumber = this.totalPages;
+        }
+
+        // set records to display on current page 
+        for (let i = (this.pageNumber - 1) * this.pageSize; i < this.pageNumber * this.pageSize; i++) {
+            if (i === this.totalRecords) {
+                break;
+            }
+            this.recordsToDisplay.push(this.records[i]);
+        }
+    }
+
+    // SORTING LOGIC
     doSorting(event) {
         this.sortBy = event.detail.fieldName;
         this.sortDirection = event.detail.sortDirection;
         this.sortData(this.sortBy, this.sortDirection);
     }
-
+    
     sortData(fieldname, direction) {
-        let parseData = JSON.parse(JSON.stringify(this.data));
+        let parseData = JSON.parse(JSON.stringify(this.records));
         // Return the value stored in the field
         let keyValue = (a) => {
             return a[fieldname];
@@ -129,6 +159,15 @@ export default class ButtonsInLWC extends NavigationMixin(LightningElement) {
             // sorting values based on direction
             return isReverse * ((x > y) - (y > x));
         });
-        this.data = parseData;
-    }    
-}  
+        this.records = parseData;
+        this.totalRecords = parseData.length; // update total records count                 
+        this.pageSize = this.pageSizeOptions[0]; //set pageSize with default value as first option
+        this.paginationHelper(); // call helper menthod to update pagination logic
+        console.log('After sorting ----',this.records);
+    } 
+
+    handleClick(event){
+       this.recordId = event.currentTarget.dataset.id;
+       console.log('@@  recordId----'+recordId);
+    }
+}
